5fc250b9eac8e367206f24c2b5dc52248c62af4e
Matthew Polak <me@matteopolak.com>
2026-09-07 17:17:19 -0400

    test(worldgen): lock heightmap packet semantics

--- crates/versions/26.2/tests/chunk_world.rs
+++ crates/versions/26.2/tests/chunk_world.rs
@@ -11,6 +11,8 @@ mod chunk_batch_ack;
 mod chunk_decode;
 #[path = "chunk_world/chunk_encode_cycles.rs"]
 mod chunk_encode_cycles;
+#[path = "chunk_world/heightmaps.rs"]
+mod heightmaps;
 #[path = "chunk_world/chunk_events.rs"]
 mod chunk_events;
 #[path = "chunk_world/chunks_biomes.rs"]
